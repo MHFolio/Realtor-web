178257699c6a83d773da89362179f21571b58b0c
manish <manishhalakatti27@gmail.com>
2024-05-21 19:01:52 +0530

    Complete the Sign in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,10 @@
 import { IoEyeOff } from "react-icons/io5";
 import { IoEye } from "react-icons/io5";
 import {useState} from 'react'
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import { signInWithEmailAndPassword,getAuth } from "firebase/auth";
+import { toast } from "react-toastify";
 
 export default function SignIn() {
   const [showPassword, setShowPassword]= useState(false);
@@ -10,9 +12,22 @@ export default function SignIn() {
     password:""
   });
   const{email,password}=formData;
+  const navigate= useNavigate();
   function onChange(e){
     setFormData((prevState)=>({...prevState,[e.target.id]:e.target.value,
     }))
+  }
+  async function onSubmit(e){
+    e.preventDefault()
+    try {
+      const auth= getAuth()
+      const userCredential= await signInWithEmailAndPassword(auth,email,password)
+      if(userCredential.user){
+        navigate("/")
+      }
+    } catch (error) {
+      toast.error("Bad user credentials")
+    }
   }
   return (
     <section>
@@ -25,7 +40,7 @@ export default function SignIn() {
           className='w-full rounded-2xl'/>
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form><input className='w-full' type="Email" id='email' value={email} onChange={onChange} placeholder='Email Address' className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out " />
+          <form onSubmit={onSubmit}><input className='w-full' type="Email" id='email' value={email} onChange={onChange} placeholder='Email Address' className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out " />
           <div className="relative mb-6">
           <input className='w-full' type={showPassword ? "text" : "password"} id='password' value={password} onChange={onChange} placeholder='Password' className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out" />{showPassword ? (<IoEyeOff className="absolute right-3 top-3 text-xl cursor-pointer" onClick={()=>setShowPassword((prevState)=>!prevState)} />) : (<IoEye className="absolute right-3 top-3 text-xl cursor-pointer" onClick={()=>setShowPassword((prevState)=>!prevState)}/>)}
           
